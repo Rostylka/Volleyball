1d2e3e5a748ed5e2549380a5695189eef8ff8101
Rostylka <rostylka96@gmail.com>
2025-04-09 11:44:30 +0300

    1 commit

---
+++ src/test/java/com/rostylka/Volleyball/VolleyballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rostylka.Volleyball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolleyballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
