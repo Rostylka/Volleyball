f713d597afd3f62eb3c4604b5cc4b21e54adf347
Rostylka <rostylka96@gmail.com>
2025-04-25 23:06:19 +0300

    Added all CRUD methods to Controller

--- src/main/java/com/rostylka/Volleyball/services/TeamPlayerService.java
+++ src/main/java/com/rostylka/Volleyball/services/TeamPlayerService.java
@@ -1,7 +1,6 @@
 package com.rostylka.Volleyball.services;
 
 import com.rostylka.Volleyball.dto.playerDto.PlayerDto;
-import com.rostylka.Volleyball.dto.teamDto.TeamDto;
 
 public interface TeamPlayerService {
 
